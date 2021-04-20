0eb1d3593088470a8629e788dee2d5d3858d8e65
Shraddha0708 <75785290+Shraddha0708@users.noreply.github.com>
2021-04-20 17:43:50 +0530

    Add files via upload

---
+++ Tree.js
@@ -0,0 +1,29 @@
+class Tree {
+
+	constructor(x,y) {
+        
+        this.x=x;
+		this.y=y;
+		this.width=450;
+		this.height=600;
+		this.thickness=10;
+		
+		this.image=loadImage("plucking+mangoes/Plucking mangoes/tree.png");
+		this.Body = Bodies.rectangle(this.x, this.y, this.width, this.thickness, {isStatic:true});
+		World.add(world, this.Body);
+
+	}
+	
+	display(){
+
+			var pos = this.Body.position;
+			push();
+			translate(pos.x, pos.y+10);
+			fill(255);
+			imageMode(CENTER);
+			image(this.image, 0,-this.height/2,this.width, this.height);
+			pop();
+			
+	}
+
+}
